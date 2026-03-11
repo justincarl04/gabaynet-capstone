b060c0fc1759024c0cf88a8019beefad7b5a90bc
JC04 <itsjustincarl04@gmail.com>
2026-03-11 19:50:53 +0800

    Util: Finished setting up winston and morgan.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./config/db');
 const helmet = require('helmet');
+const morgan = require('morgan');
+
+const pool = require('./config/db');
+const logger = require('./utils/logger');
 
 const app = express();
+
+const authRoutes = require('./modules/auth/auth.routes');
+const reportRoutes = require('./modules/reports/reports.routes');
+
+const stream = {
+    write: (message) => logger.info(message.trim()),
+};
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-const authRoutes = require('./modules/auth/auth.routes');
-const reportRoutes = require('./modules/reports/reports.routes');
+app.use(morgan('combined', { stream }));
 
 app.get('/', (req, res) => {
     res.send('API is running.');
@@ -28,5 +37,10 @@ app.get('/health', async (req, res) => {
 
 app.use('/api/reports', reportRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    logger.error(`${err.status || 500} - ${err.message} - ${err.stack} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    res.status(err.status || 500).json({ message: err.userMessage || 'Internal Server Error' });
+}); 
 
 module.exports = app;
